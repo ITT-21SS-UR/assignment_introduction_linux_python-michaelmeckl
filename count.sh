52e9d87903538a43bef9dc21e3e1deee49da767c
michael m <michael@debianITT>
2021-04-21 23:20:16 +0200

    Improve shell script

--- count.sh
+++ count.sh
@@ -7,20 +7,24 @@ FILENAME=gnome_readme.txt
 GetMostCommonWords() {
   # convert the given text to lowercase
   text_lower=$(echo "$1" | tr '[:upper:]' '[:lower:]')
-  # remove punctuation and split into separate words per line
+  # echo "$text_lower" | wc
+
+  # split text into one word per line (by replacing spaces with a newline) and remove digits, punctuation and empty lines
   # parts of the sed command were taken from https://stackoverflow.com/a/15501736 and updated to delete empty lines too
-  # TODO remove the whitespace char and maybe the numbers too!
-  text_splitted=$(echo "$text_lower" | sed -e 's/\s/\n/g ; s/[[:punct:]]*//g ; /^[[:space:]]*$/d')
+  text_splitted=$(echo "$text_lower" | tr " " "\n" | tr -d "[:digit:]" | sed -e 's/[[:punct:]]*//g ; /^[[:space:]]*$/d')
+  # echo "$text_splitted" | wc
+
   # alphabetically sort the lines and remove duplicates (the -c option is used here for the next step)
   unique_sorted=$(echo "$text_splitted" | sort | uniq -c)
   # print out the 10 most common words in the text:
+  echo "The 10 most common words in the text are (in descending order):"
   # sort based on the count in reverse order (to show the most common at the top) and use awk to remove the numbers by only printing the second field
   echo "$unique_sorted" | sort -nr | head | awk '{print $2}'
 }
 
 # the same as above as a one-liner:
 GetMostCommonWordsOneLine() {
-  echo "$1" | tr '[:upper:]' '[:lower:]' | sed -e 's/\s/\n/g ; s/[[:punct:]]*//g ; /^[[:space:]]*$/d' | sort | uniq -c | sort -nr | head | awk '{print $2}'
+  echo "$1" | tr '[:upper:]' '[:lower:]' | tr " " "\n" | tr -d "[:digit:]" | sed -e 's/[[:punct:]]*//g ; /^[[:space:]]*$/d' | sort | uniq -c | sort -nr | head | awk '{print $2}'
 }
 
 # check if the file exists in the current directory; if not download it
